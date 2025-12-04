e0403e8bcf1a3f993888e2e824423268a99e25d2
Clarent <dcheaslopez@gmail.com>
2025-12-04 15:04:31 -0400

    Página de mantenimiento - Sitio fuera de servicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,9 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { ThemeProvider } from './contexts/ThemeContext';
-import LoadingScreen from './components/LoadingScreen';
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import WhoWeAre from './components/WhoWeAre';
-import About from './components/About';
-import Committees from './components/Committees';
-import Registration from './components/Registration';
-import Gallery from './components/Gallery';
-import News from './components/News';
-import FAQ from './components/FAQ';
-import SocialMedia from './components/SocialMedia';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import MaintenancePage from './components/MaintenancePage';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleLoadingComplete = () => {
-    setIsLoading(false);
-  };
-
-  if (isLoading) {
-    return <LoadingScreen onComplete={handleLoadingComplete} />;
-  }
-
   return (
-    <ThemeProvider>
-      <div className="min-h-screen transition-colors duration-500">
-        <Navigation />
-        
-        <main className="relative">
-          <Hero />
-          <WhoWeAre />
-          <About />
-          <Committees />
-          <Registration />
-          <Gallery />
-          <News />
-          <FAQ />
-          <SocialMedia />
-          <Contact />
-        </main>
-        
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <MaintenancePage />
   );
 }
 
