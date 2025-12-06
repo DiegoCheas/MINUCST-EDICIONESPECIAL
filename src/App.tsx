bf48a697a0903307f9511510bc2cf0e34ec40f78
Clarent <dcheaslopez@gmail.com>
2025-12-05 21:02:20 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import MaintenancePage from './components/MaintenancePage';
+import React, { useEffect, useState } from 'react';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import WhoWeAre from './components/WhoWeAre';
+import About from './components/About';
+import Committees from './components/Committees';
+import Gallery from './components/Gallery';
+import News from './components/News';
+import FAQ from './components/FAQ';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import LoadingScreen from './components/LoadingScreen';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
-    <MaintenancePage />
+    <ThemeProvider>
+      <div className="min-h-screen bg-white dark:bg-gray-900">
+        <Navigation />
+        <Hero />
+        <WhoWeAre />
+        <About />
+        <Committees />
+        <Gallery />
+        <News />
+        <FAQ />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
